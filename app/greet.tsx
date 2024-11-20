8e803f3fd7f3a271346e75a35a207ef1787bf58c
baiheyufei <BaiHeYuFei@outlook.com>
2024-11-20 08:44:05 +0800

    Build a play_track button

--- app/greet.tsx
+++ app/greet.tsx
@@ -1,17 +1,39 @@
 'use client'
 
-import { useEffect, useState } from 'react';
+import { useRef } from 'react';
 import { invoke } from '@tauri-apps/api/core'
+import { open } from '@tauri-apps/plugin-dialog';
+export default function PlayTrackButton() {
+  const buttonRef = useRef<HTMLButtonElement>(null); 
 
-export default function Greet() {
-  const [greeting, setGreeting] = useState('');
+  const handleClick = async () => { 
+    if (buttonRef.current!.disabled) 
+    { 
+      return; 
+    } 
+    buttonRef.current!.disabled = true; 
+    const filePath = await open({
+      multiple: false,
+      filters: [{
+        name: 'Track',
+        extensions: ['mp3', 'flac']
+      }]
+    });
+    if (filePath !== null) {
+      // Arguments should be passed as a JSON object with CAMELCASE keys !!!!!
+      await invoke('play_track', {  filePath : filePath }); 
+    } 
 
-  useEffect(() => {
-    invoke<string>('greet', { name: 'Baihyf' })
-      .then(result => setGreeting(result))
-      .catch(console.error)
-  }, [])
+    buttonRef.current!.disabled = false; 
+  }
 
-  // Necessary because we will have to use Greet as a component later.
-  return <h2>{greeting}</h2>;
+  return (
+    <button
+      ref={buttonRef}
+      onClick={handleClick}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Play
+    </button>
+  );
 }
--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,5 +1,18 @@
+use std::io::BufReader;
+
 #[tauri::command]
-pub fn greet(name: &str) -> String {
-  println!("DEBUG: command::greet invoked.");
-  format!("Hello, {}!", name)
+pub fn play_track(file_path: &str) {
+    println!(
+        "DEBUG: command::play_track invoked with argument:\n
+            name = {}.", file_path
+    );
+
+    // Copy from https://github.com/RustAudio/rodio/blob/master/examples
+    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+    let sink = rodio::Sink::try_new(&handle).unwrap();
+
+    let file = std::fs::File::open(file_path).unwrap();
+    sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
+
+    sink.sleep_until_end();
 }
