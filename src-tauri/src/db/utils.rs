40c9d35f29be80e5a82bbefee0f373e8dc58a86c
baiheyufei <BaiHeYuFei@outlook.com>
2024-12-05 14:37:49 +0800

    feat(db): add duration parsing for MP3 files and improve tag parsing
    - Use rodio crate to parse MP3 duration
    - Update parse_mp3_tags function to return duration in seconds
    - Add unit test for parse_mp3_tags function

--- src-tauri/src/db/utils.rs
+++ src-tauri/src/db/utils.rs
@@ -1,17 +1,38 @@
 use id3::{Tag, TagLike};
+use rodio::{Decoder, OutputStream, source::Source};
+use std::io::BufReader;
+use std::fs::File;
 
 /// Parses the tags from an MP3 file.
 /// Returns the title, artist, and album name.
 pub fn parse_mp3_tags(
     path: &str,
-) -> Result<(Option<String>, Option<String>, Option<String>, Option<u32>), Box<dyn std::error::Error>>
+) -> Result<(Option<String>, Option<String>, Option<String>, Option<u64>), Box<dyn std::error::Error>>
 {
     let tag = Tag::read_from_path(path)?;
+    let file = BufReader::new(File::open(path).unwrap());
+    let source = Decoder::new(file).unwrap();
+
 
     Ok((
         tag.title().map(|s| s.to_string()),
         tag.artist().map(|s| s.to_string()),
         tag.album().map(|s| s.to_string()),
-        tag.duration(),
+        source.total_duration().map(|d| d.as_secs()),
     ))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_mp3_tags() {
+        let path = "F:/workspace/rwavetest/西木康智 - 洞窟ダンジョン.mp3";
+        let (title, artist, album, duration) = parse_mp3_tags(path).unwrap();
+        println!("Title: {:?}", title);
+        println!("Artist: {:?}", artist);
+        println!("Album: {:?}", album);
+        println!("Duration: {:?}", duration);
+    }
 }
